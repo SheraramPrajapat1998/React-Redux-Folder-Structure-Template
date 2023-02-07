dba6640013d8b205f1193e4adc8f5468ab803c71
SheraramPrajapat1998 <sheraramprajapat1998@gmail.com>
2023-02-07 09:17:37 +0530

    Revert "useDebounce hook added"
    This reverts commit 6688efd2a832670f2ddf5ce104b454d24647df47.

--- src/Screen/Dashboard/Dashboard.js
+++ src/Screen/Dashboard/Dashboard.js
@@ -1,12 +1,6 @@
-import React, { useState } from "react";
-import useDebounce from "../../Hooks/useDebounce";
+import React from "react";
 
 const Dashboard = () => {
-  const [inputValue, setInputValue] = useState("");
-
-  useDebounce(() => {
-    // make API call
-  }, 500);
   return <div>Dashboard</div>;
 };
 
